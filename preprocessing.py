0bf59245cb241d66f073e29964cd3f78c20326b6
Eltyshev Evgeny <eltyshev.e@gmail.com>
2017-11-30 20:03:11 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator
-from sklearn.cross_validation import cross_val_predict
+from sklearn.model_selection import cross_val_predict
 
 def calculate_statistics(train, test, by_field, fields, statistics=['mean']):
     '''Encodes statistics of field for every value in by_field'''
